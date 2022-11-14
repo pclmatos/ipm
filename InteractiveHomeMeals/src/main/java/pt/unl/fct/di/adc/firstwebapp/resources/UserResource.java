bfca1ed85c9e31c4ca9f23c70face18cc99d23cf
Rafael <rt.borralho@campus.fct.unl.pt>
2022-11-14 13:10:21 +0000

    added ingredients and filtering recipes

--- InteractiveHomeMeals/src/main/java/pt/unl/fct/di/adc/firstwebapp/resources/UserResource.java
+++ InteractiveHomeMeals/src/main/java/pt/unl/fct/di/adc/firstwebapp/resources/UserResource.java
@@ -34,14 +34,22 @@ public class UserResource {
 
 	private final Gson g = new Gson();
 
+	//kinds of the database
 	private static final String USER = "User";
 	private static final String RECIPE = "Recipe";
+	private static final String INGREDIENT = "Ingredient";
+	
+	//attributes
 	private static final String USERNAME= "username";
 	private static final String PASSWORD = "password";
 	private static final String AUTHOR = "author";
 	private static final String DESCRIPTION = "description";
 	private static final String INGREDIENTS = "ingredients";
 	private static final String RECIPENAME = "name";
+	private static final String ISVEGETARIAN = "isVegetarian";
+	private static final String ISVEGAN = "isVegan";
+	private static final String ISKOSHER = "isKosher";
+	private static final String ISGLUTENFREE = "isGlutenFree";
 	
 	public UserResource() {}
 	
@@ -108,11 +116,11 @@ public class UserResource {
 						LOG.info("User logged in: " + data.username);
 						return Response.ok("valid login").build();
 
-					}else {
-						LOG.warning("Wrong password for user:" + data.username);
-						txn.rollback();
-						return Response.status(Status.FORBIDDEN).entity("Wrong password.").build();
-					}
+				}else {
+					LOG.warning("Wrong password for user:" + data.username);
+					txn.rollback();
+					return Response.status(Status.FORBIDDEN).entity("Wrong password.").build();
+				}
 			}else{
 				LOG.warning("User: " + data.username +" does not exist");
 				txn.rollback();
@@ -130,12 +138,15 @@ public class UserResource {
 	public Response shareRecipe(RecipeData data) {
 		LOG.fine("Share recipe attempt by: " + data.author);
 
+		boolean isVegetarian = true, isVegan = true, isKosher = true, isGlutenFree = true;
 		Key recipeKey = datastore.newKeyFactory().setKind(RECIPE).newKey(getUniqueId());
 		Key userKey = datastore.newKeyFactory().setKind(USER).newKey(data.author);
 		
 		Transaction txn = datastore.newTransaction();
 		
 		try {
+			Key ingredientKey;
+			Entity ingredient;
 			Entity user = datastore.get(userKey);
 			Entity recipe = datastore.get(recipeKey);
 			
@@ -150,11 +161,35 @@ public class UserResource {
 				return Response.status(Status.FORBIDDEN).entity("User: " + data.author +" does not exist.").build();
 			}
 			else {
+				for(int i = 0; i < data.ingredientsToFilter.length; i++) {
+					ingredientKey = datastore.newKeyFactory().setKind(INGREDIENT).newKey(data.ingredientsToFilter[i]);
+					ingredient = datastore.get(ingredientKey);
+					
+					if(ingredient != null) {
+						if(!ingredient.getBoolean(ISVEGETARIAN)) {
+							isVegetarian = false;
+						}
+						if(!ingredient.getBoolean(ISVEGAN)) {
+							isVegan = false;
+						}
+						if(!ingredient.getBoolean(ISKOSHER)) {
+							isKosher = false;
+						}
+						if(!ingredient.getBoolean(ISGLUTENFREE)) {
+							isGlutenFree = false;
+						}
+					}
+				}
+				
 				recipe = Entity.newBuilder(recipeKey)
 					.set(RECIPENAME, data.recipeName)
 					.set(AUTHOR, data.author)
 					.set(DESCRIPTION, data.description)
 					.set(INGREDIENTS, data.ingredients)
+					.set(ISVEGETARIAN, isVegetarian)
+					.set(ISVEGAN, isVegan)
+					.set(ISKOSHER, isKosher)
+					.set(ISGLUTENFREE, isGlutenFree)
 					.build();
 			
 				txn.add(recipe);
@@ -168,6 +203,128 @@ public class UserResource {
 				txn.rollback();
 		}
 	}
+	
+	@POST
+	@Path("/addExistingIngredients")
+	public Response addExistingIngredients() {
+		LOG.fine("Adding all ingredients to db");
+		
+		String fruits[] = {"apple", "banana", "pear", "strawberry", "grape", "watermelon", "orange", "blueberry", "lemon", "peach", "avocado", "pineapple", "cherry", "cantaloupe", "raspberry", "lime", "blackberry", "clementine", "mango", "plum"};
+		String vegetables[] = {"potato", "tomato", "onion", "carrot", "bell pepper", "broccoli", "cucumber", "lettuce", "celery", "mushroom", "garlic", "spinach", "green bean", "cabbage", "sweet potato", "green onion", "cauliflower", "aspargo", "peas", "basil"};
+		String meat[] = {"pork meat", "chicken meat", "beef", "lamb meat", "goat meat", "turkey", "duck meat", "buffalo meat", "goose meat", "rabbit meat"};
+		String seaFood[] = {"shrimp", "tuna", "salmon", "tilapia", "catfish", "crab", "cod", "clam", "pangasius"};
+		String others[] = {"egg", "milk", "chocolate", "sugar", "salt", "pepper", "cinnamon", "cream", "olive oil", "tomato sauce", "soy sauce", "hot sauce", "oregano", "paprika", "curry", "cheese", "butter", "yogurt"};
+		String cereals[] = {"bread", "croissant", "grain", "oat", "rice", "pasta", "quinoa", "corn", "lentils"};
+		
+		Transaction txn = datastore.newTransaction();
+		
+		try {
+			Key ingredientKey;
+			Entity ingredient;
+			
+			for(int i = 0; i < fruits.length; i++) {
+				ingredientKey = datastore.newKeyFactory().setKind(INGREDIENT).newKey(fruits[i]);
+				ingredient = datastore.get(ingredientKey);
+				if(ingredient == null) {
+					ingredient = Entity.newBuilder(ingredientKey)
+						.set(ISVEGETARIAN, true)
+						.set(ISVEGAN, true)
+						.set(ISKOSHER, true)
+						.set(ISGLUTENFREE, true)
+						.build();
+			
+					txn.add(ingredient);
+				}
+			}
+			
+			for(int i = 0; i < vegetables.length; i++) {
+				ingredientKey = datastore.newKeyFactory().setKind(INGREDIENT).newKey(vegetables[i]);
+				ingredient = datastore.get(ingredientKey);
+				
+				if(ingredient == null) {
+					ingredient = Entity.newBuilder(ingredientKey)
+						.set(ISVEGETARIAN, true)
+						.set(ISVEGAN, true)
+						.set(ISKOSHER, true)
+						.set(ISGLUTENFREE, true)
+						.build();
+				
+					txn.add(ingredient);
+				}
+			}
+			
+			for(int i = 0; i < meat.length; i++) {
+				ingredientKey = datastore.newKeyFactory().setKind(INGREDIENT).newKey(meat[i]);
+				ingredient = datastore.get(ingredientKey);
+				
+				if(ingredient == null) {
+					ingredient = Entity.newBuilder(ingredientKey)
+						.set(ISVEGETARIAN, false)
+						.set(ISVEGAN, false)
+						.set(ISKOSHER, true)
+						.set(ISGLUTENFREE, true)
+						.build();
+				
+					txn.add(ingredient);
+				}
+			}
+			
+			for(int i = 0; i < seaFood.length; i++) {
+				ingredientKey = datastore.newKeyFactory().setKind(INGREDIENT).newKey(seaFood[i]);
+				ingredient = datastore.get(ingredientKey);
+				
+				if(ingredient == null) {
+					ingredient = Entity.newBuilder(ingredientKey)
+						.set(ISVEGETARIAN, false)
+						.set(ISVEGAN, false)
+						.set(ISKOSHER, true)
+						.set(ISGLUTENFREE, false)
+						.build();
+				
+					txn.add(ingredient);
+				}
+			}
+			
+			for(int i = 0; i < cereals.length; i++) {
+				ingredientKey = datastore.newKeyFactory().setKind(INGREDIENT).newKey(cereals[i]);
+				ingredient = datastore.get(ingredientKey);
+				
+				if(ingredient == null) {
+					ingredient = Entity.newBuilder(ingredientKey)
+						.set(ISVEGETARIAN, true)
+						.set(ISVEGAN, true)
+						.set(ISKOSHER, true)
+						.set(ISGLUTENFREE, false)
+						.build();
+				
+					txn.add(ingredient);
+				}
+			}
+			
+			for(int i = 0; i < others.length; i++) {
+				ingredientKey = datastore.newKeyFactory().setKind(INGREDIENT).newKey(others[i]);
+				ingredient = datastore.get(ingredientKey);
+				
+				if(ingredient == null) {
+					ingredient = Entity.newBuilder(ingredientKey)
+						.set(ISVEGETARIAN, true)
+						.set(ISVEGAN, true)
+						.set(ISKOSHER, true)
+						.set(ISGLUTENFREE, false)
+						.build();
+				
+					txn.add(ingredient);
+				}
+			}
+			
+			txn.commit();
+			return Response.ok("All ingredients added").build();
+		}finally {
+			if(txn.isActive())
+				txn.rollback();
+		}
+	}
+
 	
 	public String getUniqueId() {
 		String uniqueId = UUID.randomUUID().toString();
--- InteractiveHomeMeals/src/main/java/pt/unl/fct/di/adc/firstwebapp/util/RecipeData.java
+++ InteractiveHomeMeals/src/main/java/pt/unl/fct/di/adc/firstwebapp/util/RecipeData.java
@@ -1,20 +1,19 @@
 package pt.unl.fct.di.adc.firstwebapp.util;
 
-import java.util.UUID;
-
 public class RecipeData {
 	
 	public String author, recipeName, description, ingredients;
-	public long key;
+	public String[] ingredientsToFilter;
+	
 	//TODO add photograph and video
 	
 	public RecipeData() {}
 	
-	public RecipeData(String recipeName, String description, String ingredients, String author) {
+	public RecipeData(String recipeName, String description, String ingredients, String author, String[] ingredientsToFilter) {
 		author = this.author;
 		recipeName = this.recipeName;
 		description = this.description;
 		ingredients = this.ingredients;
+		ingredientsToFilter = this.ingredientsToFilter;
 	}
-
 }

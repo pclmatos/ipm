cd31ca8b84bfd7dd7719d1992cdfa7967a87681f
Paulo Matos <pauloclmatos@gmail.com>
2022-11-15 12:55:18 +0000

    added method to search for recipe based on its name

---
+++ InteractiveHomeMeals/src/main/java/pt/unl/fct/di/adc/firstwebapp/util/SearchRecipeData.java
@@ -0,0 +1,11 @@
+package pt.unl.fct.di.adc.firstwebapp.util;
+
+public class SearchRecipeData {
+    
+    public String searchKey;
+
+    public SearchRecipeData(String searchKey){
+        this.searchKey = searchKey;
+    }
+    
+}

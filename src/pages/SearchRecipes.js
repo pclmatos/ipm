27aa11a2226d7cef38ce4a259809654b462a8b2b
JoaoSergioAguero <61967521+JoaoSergioAguero@users.noreply.github.com>
2022-11-15 17:03:55 +0000

    working on search recipes

--- src/pages/SearchRecipes.js
+++ src/pages/SearchRecipes.js
@@ -1,4 +1,5 @@
-import { Box, Grid, TextField, Typography } from "@mui/material";
+import { Box, Grid, TextField, Typography, Card, CardMedia, CardContent } from "@mui/material";
+import logoIHMcut from "../images/logoIHMcut.png"
 
 export default function SearchRecipesPage() {
     return (
@@ -12,8 +13,82 @@ export default function SearchRecipesPage() {
                     <Typography sx={{ fontFamily: 'Verdana', fontSize: 20, color: "black" }}>Filtros123</Typography>
                 </Box>
             </Grid>
-            <Grid item xs={9}>
-
+            <Grid container item xs={9} direction="row">
+                <Box sx={{ p: 1, width: "33.3%" }}>
+                    <Card variant="outlined" sx={{ p: 1 }}>
+                        <CardContent >
+                            <Typography sx={{ fontFamily: 'Verdana', fontSize: 20, color: "black" }}>
+                                Receita 1
+                            </Typography>
+                        </CardContent>
+                        <CardMedia
+                            component="img"
+                            image={logoIHMcut}
+                            height="320"
+                            alt="green iguana"
+                        />
+                    </Card>
+                </Box>
+                <Box sx={{ p: 1, width: "33.3%" }}>
+                    <Card variant="outlined" sx={{ p: 1 }}>
+                        <CardContent>
+                            <Typography sx={{ fontFamily: 'Verdana', fontSize: 20, color: "black" }}>
+                                Receita 2
+                            </Typography>
+                        </CardContent>
+                        <CardMedia
+                            component="img"
+                            image={logoIHMcut}
+                            height="320"
+                            alt="green iguana"
+                        />
+                    </Card>
+                </Box>
+                <Box sx={{ p: 1, width: "33.3%" }}>
+                    <Card variant="outlined" sx={{ p: 1 }}>
+                        <CardContent >
+                            <Typography sx={{ fontFamily: 'Verdana', fontSize: 20, color: "black" }}>
+                                Receita 3
+                            </Typography>
+                        </CardContent>
+                        <CardMedia
+                            component="img"
+                            image={logoIHMcut}
+                            height="320"
+                            alt="green iguana"
+                        />
+                    </Card>
+                </Box>
+                <Box sx={{ p: 1, width: "33.3%" }}>
+                    <Card variant="outlined" sx={{ p: 1 }}>
+                        <CardContent>
+                            <Typography sx={{ fontFamily: 'Verdana', fontSize: 20, color: "black" }}>
+                                Receita 4
+                            </Typography>
+                        </CardContent>
+                        <CardMedia
+                            component="img"
+                            image={logoIHMcut}
+                            height="320"
+                            alt="green iguana"
+                        />
+                    </Card>
+                </Box>
+                <Box sx={{ p: 1, width: "33.3%" }}>
+                    <Card variant="outlined" sx={{ p: 1 }}>
+                        <CardContent>
+                            <Typography sx={{ fontFamily: 'Verdana', fontSize: 20, color: "black" }}>
+                                Receita 5
+                            </Typography>
+                        </CardContent>
+                        <CardMedia
+                            component="img"
+                            image={logoIHMcut}
+                            height="320"
+                            alt="green iguana"
+                        />
+                    </Card>
+                </Box>
             </Grid>
         </Grid>
     )

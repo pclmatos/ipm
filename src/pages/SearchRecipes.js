e9c2597552b8b69916521ae9600e2e230904fd86
JoaoSergioAguero <61967521+JoaoSergioAguero@users.noreply.github.com>
2022-11-15 12:25:13 +0000

    search recipes beginning

--- src/pages/SearchRecipes.js
+++ src/pages/SearchRecipes.js
@@ -1,5 +1,20 @@
+import { Box, Grid, TextField, Typography } from "@mui/material";
+
 export default function SearchRecipesPage() {
-    return(
-        <h1>HOJE TEM</h1>
+    return (
+        <Grid container>
+            <Grid item xs={3}>
+                <Box sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }} >
+                    <Typography sx={{ fontFamily: 'Verdana', fontSize: 20, color: "black" }}>Filtros</Typography>
+                </Box>
+            </Grid>
+            <Grid item xs={9}>
+
+            </Grid>
+        </Grid>
     )
 }

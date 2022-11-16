acc09d578e4853cf9abc323b0623df7b6c0c76d6
Diogo Ramos <dl.ramos@campus.fct.unl.pt>
2022-11-16 22:55:17 +0000

    Started consult ingredients(pantry)

--- src/pages/ConsultPantry.js
+++ src/pages/ConsultPantry.js
@@ -1,5 +1,42 @@
+import { Grid,Box, Typography, Card, CardContent, CardMedia } from "@mui/material";
+import leite from "../images/leite.jpg"
+
 export default function ConsultPantry() {
     return (
-        <h1>oi</h1>
+        <>
+        <Grid container> 
+        <Grid item xs = {3}>
+            <Box sx = {{
+                display : 'flex',
+                flexDirection : 'column',
+                alignItems: 'left'
+            }}>
+                <Typography sx = {{
+                    fontFamily: 'Verdana', fontSize: 20, color: "black" }}>Filtros </Typography>
+            </Box>
+            
+            </Grid>       
+        <Grid container item xs = {9} direction= 'row'>
+            <Box sx = {{ p:1, width : '20%'}}>
+                    <Card variant='elevation' sx = {{p:2}}>
+                    <CardContent>
+                    <Typography sx={{ fontFamily: 'Verdana', fontSize: 20, color: "black" }}>
+                                Leite
+                            </Typography>
+                    </CardContent>
+                    <CardMedia
+                        component ='img'
+                        image = {leite}
+                        height = "320"
+                        alt="green iguana"
+                    />
+                    </Card>
+            </Box>
+
+        </Grid>
+
+        </Grid>
+        
+        </> 
     )
 }

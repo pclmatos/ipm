8f23077b32b503cc807dc17e828147215fd8d46c
JoaoSergioAguero <61967521+JoaoSergioAguero@users.noreply.github.com>
2022-11-17 15:51:57 +0000

    ingredient filtering

--- src/pages/SearchRecipes.js
+++ src/pages/SearchRecipes.js
@@ -1,4 +1,5 @@
 import { Box, Grid, TextField, Typography, Card, CardMedia, CardContent, RadioGroup, FormControlLabel, Radio, ThemeProvider, createTheme } from "@mui/material";
+import Select from "react-select"
 import logoIHMcut from "../images/logoIHMcut.png"
 import { useState } from "react"
 
@@ -8,7 +9,7 @@ export default function SearchRecipesPage() {
     const [kosher, setKosher] = useState("false")
     const [glutenFree, setGlutenFree] = useState("false")
     const [lactoseFree, setLactoseFree] = useState("false")
-    const [ingredients, setIngredients] = useState("")
+    const [selectedMeatFishEggsIngredients, setSelectedMeatFishEggsIngredients] = useState()
 
     function vegetarianHandler(e) {
         if (vegetarian === "true") {
@@ -50,9 +51,99 @@ export default function SearchRecipesPage() {
         }
     }
 
-    function ingredientsHandler(e) {
-        setIngredients(e.target.value);
+    function selectedMeatFishEggsIngredientsHandler(data) {
+        setSelectedMeatFishEggsIngredients(data);
     }
+
+    const meatFishEggsOptionList = [
+        { value: "apple", label: "Apple" },
+        { value: "banana", label: "Banana" },
+        { value: "pear", label: "Pear" },
+        { value: "strawberry", label: "Strawberry" },
+        { value: "grape", label: "Grape" },
+        { value: "watermelon", label: "Watermelon" },
+        { value: "orange", label: "Orange" },
+        { value: "blueberry", label: "Blueberry" },
+        { value: "lemon", label: "Lemon" },
+        { value: "peach", label: "Peach" },
+        { value: "avocado", label: "Avocado" },
+        { value: "pineapple", label: "Pineapple" },
+        { value: "cherry", label: "Cherry" },
+        { value: "cantaloupe", label: "Cantaloupe" },
+        { value: "raspberry", label: "Raspberry" },
+        { value: "lime", label: "Lime" },
+        { value: "blackberry", label: "Blackberry" },
+        { value: "clementine", label: "Clementine" },
+        { value: "mango", label: "Mango" },
+        { value: "plum", label: "Plum" },
+        { value: "kiwi", label: "Kiwi" },
+        { value: "potato", label: "Potato" },
+        { value: "tomato", label: "Tomato" },
+        { value: "onion", label: "Onion" },
+        { value: "carrot", label: "Carrot" },
+        { value: "bell_pepper", label: "Bell Pepper" },
+        { value: "broccoli", label: "Broccoli" },
+        { value: "cucumber", label: "Cucumber" },
+        { value: "lettuce", label: "Lettuce" },
+        { value: "celery", label: "Celery" },
+        { value: "mushroom", label: "Mushroom" },
+        { value: "garlic", label: "Garlic" },
+        { value: "spinach", label: "Spinach" },
+        { value: "green_bean", label: "Green Bean" },
+        { value: "cabbage", label: "Cabbage" },
+        { value: "sweet_potato", label: "Sweet Potato" },
+        { value: "green_onion", label: "Green Onion" },
+        { value: "cauliflower", label: "Cauliflower" },
+        { value: "aspargo", label: "Aspargo" },
+        { value: "peas", label: "Peas" },
+        { value: "basil", label: "Basil" },
+        { value: "pork", label: "Pork" },
+        { value: "chicken", label: "Chicken" },
+        { value: "beef", label: "Beef" },
+        { value: "lamb", label: "Lamb" },
+        { value: "goat", label: "Goat" },
+        { value: "turkey", label: "Turkey" },
+        { value: "duck", label: "Duck" },
+        { value: "buffalo", label: "Buffalo" },
+        { value: "goose", label: "Goose" },
+        { value: "rabbit", label: "Rabbit" },
+        { value: "shrimp", label: "Shrimp" },
+        { value: "tuna", label: "Tuna" },
+        { value: "salmon", label: "Salmon" },
+        { value: "tilapia", label: "Tilapia" },
+        { value: "catfish", label: "Catfish" },
+        { value: "crab", label: "Crab" },
+        { value: "cod", label: "Cod" },
+        { value: "clam", label: "Clam" },
+        { value: "pangasius", label: "Pangasius" },
+        { value: "egg", label: "Egg" },
+        { value: "milk", label: "Milk" },
+        { value: "chocolate", label: "Chocolate" },
+        { value: "sugar", label: "Sugar" },
+        { value: "salt", label: "Salt" },
+        { value: "pepper", label: "Pepper" },
+        { value: "cinnamon", label: "Cinnamon" },
+        { value: "cream", label: "Cream" },
+        { value: "olive_oil", label: "Olive Oil" },
+        { value: "tomato_sauce", label: "Tomato Sauce" },
+        { value: "soy_sauce", label: "Soy Sauce" },
+        { value: "hot_sauce", label: "Hot Sauce" },
+        { value: "oregano", label: "Oregano" },
+        { value: "paprika", label: "Paprika" },
+        { value: "curry", label: "Curry" },
+        { value: "cheese", label: "Cheese" },
+        { value: "butter", label: "Butter" },
+        { value: "yogurt", label: "Yogurt" },
+        { value: "bread", label: "Bread" },
+        { value: "croissant", label: "Croissant" },
+        { value: "grain", label: "Grain" },
+        { value: "oat", label: "Oat" },
+        { value: "rice", label: "Rice" },
+        { value: "pasta", label: "Pasta" },
+        { value: "quinoa", label: "Quinoa" },
+        { value: "corn", label: "Corn" },
+        { value: "lentils", label: "Lentils" },
+    ];
 
     const theme = createTheme({
         palette: {
@@ -70,34 +161,45 @@ export default function SearchRecipesPage() {
                     flexDirection: 'column',
                     alignItems: 'center',
                 }} >
-                    <Typography sx={{ fontFamily: 'Verdana', fontSize: 25, color: "black", pt: "6%", pb: "2%" }}>Filtragem de Receitas</Typography>
-                    <ThemeProvider theme={theme}>
-                        <RadioGroup
-                            value={vegetarian}
-                            onClick={vegetarianHandler}>
-                            <FormControlLabel value="true" control={<Radio />} label={<Typography sx={{ fontFamily: 'Verdana', fontSize: 18 }}>Vegetarian</Typography>} />
-                        </RadioGroup>
-                        <RadioGroup
-                            value={vegan}
-                            onClick={veganHandler}>
-                            <FormControlLabel value="true" control={<Radio />} label={<Typography sx={{ fontFamily: 'Verdana', fontSize: 18 }}>Vegan</Typography>} />
-                        </RadioGroup>
-                        <RadioGroup
-                            value={kosher}
-                            onClick={kosherHandler}>
-                            <FormControlLabel value="true" control={<Radio />} label={<Typography sx={{ fontFamily: 'Verdana', fontSize: 18 }}>Kosher</Typography>} />
-                        </RadioGroup>
-                        <RadioGroup
-                            value={glutenFree}
-                            onClick={glutenFreeHandler}>
-                            <FormControlLabel value="true" control={<Radio />} label={<Typography sx={{ fontFamily: 'Verdana', fontSize: 18 }}>Gluten Free</Typography>} />
-                        </RadioGroup>
-                        <RadioGroup
-                            value={lactoseFree}
-                            onClick={lactoseFreeHandler}>
-                            <FormControlLabel value="true" control={<Radio />} label={<Typography sx={{ fontFamily: 'Verdana', fontSize: 18 }}>Lactose Free</Typography>} />
-                        </RadioGroup>
-                    </ThemeProvider>
+                    <Typography sx={{ fontFamily: 'Verdana', fontSize: 25, color: "black", pt: "6%", pb: "1%" }}>Diet Filtering</Typography>
+                    <Box sx={{ pl: "6%" }}>
+                        <ThemeProvider theme={theme}>
+                            <RadioGroup
+                                value={vegetarian}
+                                onClick={vegetarianHandler}>
+                                <FormControlLabel value="true" control={<Radio />} label={<Typography sx={{ fontFamily: 'Verdana', fontSize: 18 }}>Vegetarian</Typography>} />
+                            </RadioGroup>
+                            <RadioGroup
+                                value={vegan}
+                                onClick={veganHandler}>
+                                <FormControlLabel value="true" control={<Radio />} label={<Typography sx={{ fontFamily: 'Verdana', fontSize: 18 }}>Vegan</Typography>} />
+                            </RadioGroup>
+                            <RadioGroup
+                                value={kosher}
+                                onClick={kosherHandler}>
+                                <FormControlLabel value="true" control={<Radio />} label={<Typography sx={{ fontFamily: 'Verdana', fontSize: 18 }}>Kosher</Typography>} />
+                            </RadioGroup>
+                            <RadioGroup
+                                value={glutenFree}
+                                onClick={glutenFreeHandler}>
+                                <FormControlLabel value="true" control={<Radio />} label={<Typography sx={{ fontFamily: 'Verdana', fontSize: 18 }}>Gluten Free</Typography>} />
+                            </RadioGroup>
+                            <RadioGroup
+                                value={lactoseFree}
+                                onClick={lactoseFreeHandler}>
+                                <FormControlLabel value="true" control={<Radio />} label={<Typography sx={{ fontFamily: 'Verdana', fontSize: 18 }}>Lactose Free</Typography>} />
+                            </RadioGroup>
+                        </ThemeProvider>
+                    </Box>
+                    <Typography sx={{ fontFamily: 'Verdana', fontSize: 25, color: "black", pt: "5%", pb: "2%" }}>Ingredient Filtering</Typography>
+                    <Select
+                        options={meatFishEggsOptionList}
+                        placeholder="Select ingredients"
+                        value={selectedMeatFishEggsIngredients}
+                        onChange={selectedMeatFishEggsIngredientsHandler}
+                        isSearchable={true}
+                        isMulti
+                    />
                 </Box>
             </Grid>
             <Grid container item xs={9} direction="row">

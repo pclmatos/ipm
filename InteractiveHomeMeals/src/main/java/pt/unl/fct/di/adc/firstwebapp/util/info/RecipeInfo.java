9c583e3cfdb9dff9ec7e36a2615b507e26b3e9d2
Paulo Matos <pauloclmatos@gmail.com>
2022-11-18 12:19:36 +0000

    changed ingredient type to String

--- InteractiveHomeMeals/src/main/java/pt/unl/fct/di/adc/firstwebapp/util/info/RecipeInfo.java
+++ InteractiveHomeMeals/src/main/java/pt/unl/fct/di/adc/firstwebapp/util/info/RecipeInfo.java
@@ -2,10 +2,12 @@ package pt.unl.fct.di.adc.firstwebapp.util.info;
 
 public class RecipeInfo {
 	
-	public String author, calories, category, description, difficulty, ingredients, name, id;
+	public String author, category, description, ingredients, name, id;
+	public long difficulty, calories;
 	public boolean isLactoseFree, isGlutenFree, isVegan, isVegetarian, isKosher;
 	
-	public RecipeInfo(String id, String author, String calories, String category, String description, String difficulty, String ingredients, String name, boolean isGlutenFree, boolean isKosher, boolean isLactoseFree, boolean isVegan, boolean isVegetarian) {
+	public RecipeInfo(String id, String author, long calories, String category, String description, long difficulty, String ingredients, 
+			String name, boolean isGlutenFree, boolean isKosher, boolean isLactoseFree, boolean isVegan, boolean isVegetarian) {
 		this.id = id;
 		this.author = author;
 		this.calories = calories;
